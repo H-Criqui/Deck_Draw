7f93e2f7dc62449b5e0556c31fdc0b55cbc73af1
H-Criqui <hec26@humboldt.edu>
2020-02-28 22:02:37 -0800

    added reflection

---
+++ DeckOfCards/Assets/Scripts/DrawTop.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawTop : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public GameObject manager;
+    public void OnClick()
+    {
+        manager.GetComponent<GameManager>().DrawTopCard();
+    }
+}
